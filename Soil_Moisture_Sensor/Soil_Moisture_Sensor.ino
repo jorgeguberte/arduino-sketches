05c9274885ca8d18cbe99b99a72f3e3045872eff
Jorge Guberte <jorgeguberte@gmail.com>
2017-03-25 08:23:49 -0300

    Soil moisture sensor sketch rewriting

--- Soil_Moisture_Sensor/Soil_Moisture_Sensor.ino
+++ Soil_Moisture_Sensor/Soil_Moisture_Sensor.ino
@@ -1,13 +1,26 @@
+#define sensorPin A0
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue =   analogRead(A0);
-  int mappedValue = map(sensorValue, 1024, 0, 0, 100);
-  String partOne = "Umidade do solo: ";
-  String output = partOne+mappedValue+"%";
-  Serial.println(output);
+  String outputMessage = "Umidade do solo: ";
+  Serial.println(outputMessage+getSoilMoisture());
   delay(2000);
+}
+
+/* Gets percentage integer from the sensor data and convert it to a string*/
+String getSoilMoisture(){
+  String soilMoisture = String(getSensorData())+"%";
+  return soilMoisture;
+}
+
+/* Fetches raw data from the sensor and map it to percentage values*/
+int getSensorData(){
+  int sensorValue =   analogRead(sensorPin);
+  
+  int mappedSensorValue = map(sensorValue, 1024, 0, 0, 100);
+  return mappedSensorValue;
 }
 
